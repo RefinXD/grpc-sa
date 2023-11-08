1bb8eac8fc7d429834b5243aef4416ff53766b42
RefinXD <The32069@gmail.com>
2023-11-08 22:50:50 +0700

    Revert "test"
    This reverts commit 1c79c68708aeb400d9d8cedfbb074f1ceb7c7c57.

--- client/client.go
+++ client/client.go
@@ -36,7 +36,6 @@ func main(){
 		Addr: ":8080",
 		Handler:r,
 	}
-	log.Printf("Client running at 8080")
 	if err:= server.ListenAndServe();err != nil{
 		panic(err)
 	}
--- script.sh
+++
@@ -1,2 +0,0 @@
-go run server/server.go &
-go run client/client.go
